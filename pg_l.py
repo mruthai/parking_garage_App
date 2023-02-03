c2960b230d0260e82ebebb6ed240c3e1cf5169a9
luamgebremikael <123391464+luamgebremikael@users.noreply.github.com>
2023-02-03 16:22:51 -0500

    luams code part 1

--- pg_l.py
+++ pg_l.py
@@ -1,1 +1,38 @@
-# test 2
+
+#Your parking gargage class should have the following methods:
+# takeTicket
+# This should decrease the amount of tickets available by 1
+# This should decrease the amount of parkingSpaces available by 1
+# payForParking
+# Display an input that waits for an amount from the user and store it in a variable
+ #If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
+# This should update the "currentTicket" dictionary key "paid" to True
+#leaveGarage
+ #If the ticket has been paid, display a message of "Thank You, have a nice day"
+ #If the ticket has not been paid, display an input prompt for payment
+ #Once paid, display message "Thank you, have a nice day!"
+ #Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
+ #Update tickets list to increase by 1 (meaning add to the tickets list)
+
+#You will need a few attributes as well:- tickets -> list
+ #parkingSpaces -> list
+ #currentTicket -> dictionary
+class Ticket_container():
+    def __init__(self):
+        self.ticket=
+        self.pk_space=
+        self.currentTicket={}
+
+    def take_ticket(self):
+        #tickrt = input("")   
+
+
+        take_ticket =product (ticket) 
+        self.ticket.append(take_ticket)
+
+    def p_space(self):
+        slot = 200
+       for i in range (self.pk_space)
+    =
+
+
